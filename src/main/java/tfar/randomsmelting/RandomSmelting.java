6eb3c998dbc3d02ba4228c9d7bc6c0255d7e036e
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2021-03-15 03:18:15 -0400

    a

---
+++ src/main/java/tfar/randomsmelting/RandomSmelting.java
@@ -0,0 +1,19 @@
+package tfar.randomsmelting;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(RandomSmelting.MODID)
+public class RandomSmelting
+{
+    // Directly reference a log4j logger.
+
+    public static final String MODID = "randomsmelting";
+
+    public RandomSmelting() {
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+}
---
+++ src/main/java/tfar/randomsmelting/mixin/RecipeManagerMixin.java
@@ -0,0 +1,28 @@
+package tfar.randomsmelting.mixin;
+
+import com.google.gson.JsonObject;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.item.crafting.RecipeManager;
+import net.minecraft.profiler.IProfiler;
+import net.minecraft.resources.IResourceManager;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tfar.randomsmelting.MixinHooks;
+
+import java.util.Map;
+
+@Mixin(RecipeManager.class)
+public class RecipeManagerMixin {
+	@Shadow private Map<IRecipeType<?>, Map<ResourceLocation, IRecipe<?>>> recipes;
+
+	@Inject(method = "apply",at = @At("RETURN"))
+	public void scrambleRecipes(Map<ResourceLocation, JsonObject> splashList, IResourceManager resourceManagerIn, IProfiler profilerIn, CallbackInfo ci) {
+		if (/*RecipeRandomizer.ServerConfig.random_crafting.get()*/ true)
+		recipes = MixinHooks.scramble(recipes);
+	}
+}
